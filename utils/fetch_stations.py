ebc48f0f32df38c2497c80eaf68907f60c063880
Jack Doyle <jack.doyle.environment@gmail.com>
2023-03-19 18:58:39 +0100

    Added function plus pandas output

--- utils/fetch_stations.py
+++ utils/fetch_stations.py
@@ -1,15 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
+def read_stations(url: str, table_name: str) -> pd.DataFrame: 
+    url = url
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table', id="REALPRECIP_LIST")
+    df = pd.read_html(str(table))[0]
+    return df
 
 url = "https://cdec.water.ca.gov/reportapp/javareports?name=DailyPrecip"
 
-response = requests.get(url)
+table_name = "REALPRECIP_LIST"
 
-print(response.status_code)
+station_data = read_stations(url, table_name)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-
-table = soup.find('table', id="REALPRECIP_LIST")
-
-print(table)
+print(station_data.head())
 
