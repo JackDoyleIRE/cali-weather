3f5a2439a7cece4e3c21c706d0e68ea0b4267d30
Jack Doyle <jack.doyle.environment@gmail.com>
2023-05-12 18:23:39 +0200

    Refactor bucket upload

--- infrastructure/load_rainfall_to_gcp_bucket.py
+++ infrastructure/load_rainfall_to_gcp_bucket.py
@@ -2,83 +2,124 @@ from google.cloud import bigquery
 from google.cloud import storage
 import requests
 import json
+from typing import List, Dict, Any
 
-# Create a BigQuery client object.
-bq_client = bigquery.Client(project="tranquil-gasket-374723")
+# Define constants
+PROJECT_ID = "tranquil-gasket-374723"
+TABLE_ID = "tranquil-gasket-374723.cali_weather.weather_stations"
+URL = "https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet"
+BUCKET_NAME = "cali-rainfall-data-analysis"
+OBJECT_NAME = "rainfall-data.json"
+START_DATE = "2020-01-01"
+END_DATE = "2023-04-01"
 
-# Set the ID of the table containing the weather station information.
-table_id = "tranquil-gasket-374723.cali_weather.weather_stations"
+def bq_client() -> bigquery.Client:
+    return bigquery.Client(project=PROJECT_ID)
 
-# Define the API endpoint URL and parameters
-url = "https://cdec.water.ca.gov/dynamicapp/req/JSONDataServlet"
+def storage_client() -> storage.Client:
+    return storage.Client(project=PROJECT_ID)
 
-# Set the name of your bucket and the name of the object you want to create
-bucket_name = "cali-rainfall-data-analysis"
-object_name = "rainfall-data.json"
+def get_station_ids(client: bigquery.Client) -> List[str]:
+    query = f"""
+        SELECT DISTINCT id
+        FROM `{TABLE_ID}`
+        ORDER BY id
+    """
+    query_job = client.query(query)
+    results = query_job.result()
+    return [row.id for row in results]
 
-# Define the date range for the API calls
-start_date = "2020-01-01"
-end_date = "2023-04-01"
-
-# Construct a SQL query to retrieve the weather station IDs.
-query = f"""
-    SELECT DISTINCT id
-    FROM `{table_id}`
-    ORDER BY id
-"""
-
-# Execute the query and retrieve the results.
-query_job = bq_client.query(query)
-results = query_job.result()
-
-# Extract the weather station IDs into a Python list.
-station_ids = [row.id for row in results]
-
-# Make API calls with a limit of 50 stations per call
-for i in range(0, len(station_ids), 50):
-    # Create a comma-separated string of station IDs for this API call
-    station_ids_str = ",".join(station_ids[i:i+50])
-
-    # Update the params dictionary with the new values
+def fetch_data(station_ids_str: str) -> Dict[str, Any]:
     params = {
         "Stations": station_ids_str,
         "SensorNums": "2",
         "dur_code": "D",
-        "Start": start_date,
-        "End": end_date
+        "Start": START_DATE,
+        "End": END_DATE
     }
+    response = requests.get(URL, params=params)
+    return json.loads(response.text)
 
-    # Send a GET request to the API endpoint and get the JSON response
-    response = requests.get(url, params=params)
-
-    # Parse the JSON response
-    data = json.loads(response.text)
-
-    # Save the JSON response to a file
-    with open(object_name, "a") as f:
+def store_data(data: Dict[str, Any], filename: str) -> None:
+    with open(filename, "a") as f:
         for item in data:
             json.dump(item, f)
             f.write('\n')
 
-with open('rainfall-data.json') as f:
-    data = f.readlines()
+def process_data(filename: str) -> None:
+    with open(filename) as f:
+        data = f.readlines()
+    processed_data = [process_line(line) for line in data]
+    with open(filename, 'w') as f:
+        for line in processed_data:
+            f.write(line + '\n')
 
-for i, line in enumerate(data):
+def process_line(line: str) -> str:
     json_data = json.loads(line)
     date = json_data["date"]
     obs_date = json_data["obsDate"]
     json_data["date"] = f"{date}:00"
     json_data["obsDate"] = f"{obs_date}:00"
-    data[i] = json.dumps(json_data)
+    return json.dumps(json_data)
 
-with open('rainfall-data.json', 'w') as f:
-    for line in data:
-        f.write(json.dumps(json.loads(line)) + '\n')
+def upload_file(client: storage.Client, bucket_name: str, object_name: str, filename: str) -> None:
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(filename)
+    print(f"File {object_name} uploaded to bucket {bucket_name}")
 
-# Upload the file to Google Cloud Storage
-storage_client = storage.Client(project="tranquil-gasket-374723")
-bucket = storage_client.bucket(bucket_name)
-blob = bucket.blob(object_name)
-blob.upload_from_filename(object_name)
+def main() -> None:
+    try:
+        bq = bq_client()
+        print("BigQuery client created successfully.")
+    except Exception as e:
+        print(f"Error creating BigQuery client: {e}")
+        return
 
-print(f"File {object_name} uploaded to bucket {bucket_name}")
+    try:
+        storage = storage_client()
+        print("Storage client created successfully.")
+    except Exception as e:
+        print(f"Error creating Storage client: {e}")
+        return
+
+    try:
+        station_ids = get_station_ids(bq)
+        print("Weather station IDs retrieved successfully.")
+    except Exception as e:
+        print(f"Error retrieving weather station IDs: {e}")
+        return
+
+    for i in range(0, len(station_ids), 50):
+        station_ids_str = ",".join(station_ids[i:i+50])
+
+        try:
+            data = fetch_data(station_ids_str)
+            print(f"Data fetched successfully for stations {station_ids_str}.")
+        except Exception as e:
+            print(f"Error fetching data for stations {station_ids_str}: {e}")
+            continue
+
+        try:
+            store_data(data, OBJECT_NAME)
+            print(f"Data stored successfully for stations {station_ids_str}.")
+        except Exception as e:
+            print(f"Error storing data for stations {station_ids_str}: {e}")
+            continue
+
+    try:
+        process_data(OBJECT_NAME)
+        print("Data processing completed successfully.")
+    except Exception as e:
+        print(f"Error processing data: {e}")
+        return
+
+    try:
+        upload_file(storage, BUCKET_NAME, OBJECT_NAME, OBJECT_NAME)
+        print("Data uploaded successfully to Google Cloud Storage.")
+    except Exception as e:
+        print(f"Error uploading data to Google Cloud Storage: {e}")
+
+if __name__ == "__main__":
+    main()
+
